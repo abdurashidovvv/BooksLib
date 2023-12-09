42e249828bfb593cab78edd7bd185228ff2373a8
Saydullo Abdurashidov <abdurashidovsaydullo35@gmail.com>
2023-12-09 15:13:55 +0300

    1.0 SplashScreenFragment

--- app/src/main/java/uz/abdurashidov/bookapp/MainActivity.kt
+++ app/src/main/java/uz/abdurashidov/bookapp/MainActivity.kt
@@ -2,10 +2,13 @@ package uz.abdurashidov.bookapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import uz.abdurashidov.bookapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
