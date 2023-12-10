fda7fd63d6db01ea3d4c69f0d01f692b2432e520
Saydullo Abdurashidov <abdurashidovsaydullo35@gmail.com>
2023-12-10 08:36:21 +0300

    3.0 OnboardingItemFragment

--- app/src/main/java/uz/abdurashidov/bookapp/presentation/onboarding_screen/OnboardingAdapter.kt
+++ app/src/main/java/uz/abdurashidov/bookapp/presentation/onboarding_screen/OnboardingAdapter.kt
@@ -3,13 +3,13 @@ package uz.abdurashidov.bookapp.presentation.onboarding_screen
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
-class OnboardingApdapter(val list: List<String>, fragment: Fragment)
+class OnboardingAdapter(val list: List<String>, fragment: Fragment)
     : FragmentStateAdapter(fragment){
     override fun getItemCount(): Int {
         return list.size
     }
 
     override fun createFragment(position: Int): Fragment {
-        return ImageFragment.newInstance(list[position])
+        return OnboardingItemFragment.newInstance(list[position])
     }
 }
--- app/src/main/java/uz/abdurashidov/bookapp/presentation/onboarding_screen/OnboardingItemFragment.kt
+++ app/src/main/java/uz/abdurashidov/bookapp/presentation/onboarding_screen/OnboardingItemFragment.kt
@@ -6,11 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import uz.abdurashidov.bookapp.R
+import uz.abdurashidov.bookapp.databinding.FragmentOnboardingItemBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -20,22 +20,21 @@ private const val ARG_PARAM2 = "param2"
 class OnboardingItemFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
         }
     }
 
+    private val binding by lazy { FragmentOnboardingItemBinding.inflate(layoutInflater) }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_onboarding_item, container, false)
+    ): View {
+
+        return binding.root
     }
 
     companion object {
@@ -44,16 +43,14 @@ class OnboardingItemFragment : Fragment() {
          * this fragment using the provided parameters.
          *
          * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
          * @return A new instance of fragment OnboardingItemFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance(param1: String) =
             OnboardingItemFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
                 }
             }
     }
